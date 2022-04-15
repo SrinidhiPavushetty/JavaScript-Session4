2cc407511afd79aaf9d26d098693211a0573e6d9
SrinidhiPavushetty <pavushettysrinidhi@gmail.com>
2022-04-15 20:14:12 +0530

    added script

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-var today = new Date()
-  var day = today.getDay()
-var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"]
-  console.log("Today is : " + daylist[day] + ".")
+function dayofweek(d, m, y){
+    let t = [ 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 ];
+    y -= (m < 3) ? 1 : 0;
+    return ( y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
+}
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+let day = dayofweek(09, 02, 2022);
+console.log(day);
+console.log(daylist[3]);
